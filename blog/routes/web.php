864e470a8ab833c8c1cb899632856f29e8d5e272
bakhtaouiines <inesbkht@gmail.com>
2022-01-31 17:00:30 +0100

    configuration of routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,3 +16,15 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+// API route group
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('register', 'AuthController@register');
+    $router->post('login', 'AuthController@login');
+    
+    $router->get('profile', 'UserController@profile');
+
+    //get one user by id
+    $router->get('users/{id}', 'UserController@singleUser');
+    $router->get('users', 'UserController@allUsers');
+});
