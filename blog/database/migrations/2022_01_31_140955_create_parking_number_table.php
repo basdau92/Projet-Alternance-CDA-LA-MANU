2dc49f3ab4855008fc4b5a547737f0fe4c831b62
taslima <prenom.nom@epitech.eu>
2022-01-31 15:22:25 +0100

    Property migrations 1/2

---
+++ blog/database/migrations/2022_01_31_140955_create_parking_number_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParkingNumberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parking_number', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->foreignId('id_annexe')->constrained('annexe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parking_number');
+    }
+}
