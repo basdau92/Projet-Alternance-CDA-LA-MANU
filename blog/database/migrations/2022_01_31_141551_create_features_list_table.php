c13b8e8548b6865494611c1805f7c6589fa432bd
bakhtaouiines <81554150+bakhtaouiines@users.noreply.github.com>
2022-01-31 16:41:08 +0100

    Update 2022_01_31_141551_create_features_list_table.php

--- blog/database/migrations/2022_01_31_141551_create_features_list_table.php
+++ blog/database/migrations/2022_01_31_141551_create_features_list_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFeatureListTable extends Migration
+class CreateFeaturesListTable extends Migration
 {
     /**
      * Run the migrations.
