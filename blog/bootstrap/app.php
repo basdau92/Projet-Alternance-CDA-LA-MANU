b580ccf2c2df4e5a54791e23e1fe58b6415377d8
bakhtaouiines <inesbkht@gmail.com>
2022-02-01 16:02:03 +0100

    added new dependencies and configuration of LumenGenerator

--- blog/bootstrap/app.php
+++ blog/bootstrap/app.php
@@ -94,6 +94,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
