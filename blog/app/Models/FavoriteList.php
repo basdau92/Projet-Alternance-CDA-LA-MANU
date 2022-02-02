0b399c7cb5a75ec76670a71a8e155bbcba5c364d
bakhtaouiines <inesbkht@gmail.com>
2022-02-02 14:50:13 +0100

    still working on relationships

--- blog/app/Models/FavoriteList.php
+++ blog/app/Models/FavoriteList.php
@@ -2,25 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class FavoriteList extends Model
+
+class FavoriteList extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
+    use Authenticatable, Authorizable;
+
     protected $table = 'favorite_list';
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 
     /**
      * get the favorite's properties list
      */
     public function hasManyProperties()
     {
-        return $this->hasMany(Property::class , 'favorite_list.id_property', 'id_client');
+        return $this->hasMany(Property::class);
     }
-
-    // public function favoriteList()
-    // {
-    //     $favorites = DB::table('favorite_list')
-    //                      ->leftJoin('client', 'favorite_list.id_client', '=', 'client.id')
-    //                      ->leftJoin('property', 'favorite_list.id_property', '=', 'property.id')
-    //                      ->get();
-    // }
 }
