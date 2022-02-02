c3adfb2e8ca4ec61e13c56a360d44a49da2bdd6a
bakhtaouiines <inesbkht@gmail.com>
2022-02-02 16:31:55 +0100

    success for join relationship

--- blog/app/Models/Property.php
+++ blog/app/Models/Property.php
@@ -2,15 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\FavoriteList;
 use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
     protected $table = 'property';
-
-    public function belongsToFavoriteList()
-    {
-        return $this->hasMany(FavoriteList::class, 'id_property', 'id');
-    }
 }
