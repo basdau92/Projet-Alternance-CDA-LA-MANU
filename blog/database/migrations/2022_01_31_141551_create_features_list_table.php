c21336b4a5d58129e1ccaaad54db54a539f85c08
Bastien Daugenne <bastien.daugenne@gmail.com>
2022-02-02 10:00:47 +0100

    Push corrections migrations 02-02

--- blog/database/migrations/2022_01_31_141551_create_features_list_table.php
+++ blog/database/migrations/2022_01_31_141551_create_features_list_table.php
@@ -15,10 +15,10 @@ class CreateFeaturesListTable extends Migration
     {
         Schema::create('features_list', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id_hygiene')->constrained('hygiene');
-            $table->foreignId('id_outdoor')->constrained('outdoor');
+            $table->foreignId('id_hygiene')->nullable()->constrained('hygiene');
+            $table->foreignId('id_outdoor')->nullable()->constrained('outdoor');
             $table->foreignId('id_property')->constrained('property');
-            $table->foreignId('id_annexe')->constrained('annexe');
+            $table->foreignId('id_annexe')->nullable()->constrained('annexe');
             $table->timestamps();
         });
     }
