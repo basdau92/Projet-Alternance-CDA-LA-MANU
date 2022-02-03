10944fe4ea601731bb9da3fc8b09f9756a4a8da2
taslima <prenom.nom@epitech.eu>
2022-02-03 17:04:34 +0100

    Find max table annexe, outdoor, hygien

--- blog/app/Http/Controllers/PropertyController.php
+++ blog/app/Http/Controllers/PropertyController.php
@@ -32,9 +32,8 @@ class PropertyController extends Controller
        $this->middleware('auth:api');
     }
 
-    public function compareSizeArray($array1,$array2,$array3)
+    public function compareSizeArray($tab)
     {
-        $tab = [count($array1),count($array2),count($array3)];
         $value = max($tab);
         $key = array_search($value,$tab);
         return $key;   
@@ -85,45 +84,51 @@ class PropertyController extends Controller
             $hygienes = unserialize($request->input('hygiene'));
             $outdoors = unserialize($request->input('outdoor'));
             $annexes = unserialize($request->input('annexe'));
-            //dd($annexes);
-
-            //$maxTab = $this->compareSizeArray($hygienes,$outdoors,$annexes);
             
-            $propertyType->save();
-            $property->id_property_type = $propertyType->id;
+            // $propertyType->save();
+            // $property->id_property_type = $propertyType->id;
             
-            $property->save();
+            // $property->save();
 
-            foreach($rooms as $r)
-            {
-                $room = new Room();
-                $room->id_property = $property->id;
+            // foreach($rooms as $r)
+            // {
+            //     $room = new Room();
+            //     $room->id_property = $property->id;
 
-                $room->id_room_type = $r;
-                $room->save();
-            }
+            //     $room->id_room_type = $r;
+            //     $room->save();
+            // }
 
-            for($i=0;$i<5;$i++)
-            {
-                $featuresList = new FeaturesList();
-                $featuresList->id_property = $property->id;
+            //$tab = [$annexes,$outdoors,$hygienes];
+            $tab = ['annexe'=>$annexes,'outdoor'=>$outdoors,'hygiene'=>$hygienes];
 
-                $featuresList->id_annexe = $annexes[$i];
+            $max = $this->compareSizeArray($tab);
+            dd($max);
 
-                if(array_key_exists($i,$outdoors))
-                {
-                    $featuresList->id_outdoor = $outdoors[$i];
-                }
-                if(array_key_exists($i,$hygienes))
-                {
-                   $featuresList->id_hygiene = $hygienes[$i];
-                }
-                $featuresList->save();
 
-            }
+
+
+            // for($i=0;$i<5;$i++)
+            // {
+            //     $featuresList = new FeaturesList();
+            //     $featuresList->id_property = $property->id;
+
+            //     $featuresList->id_annexe = $annexes[$i];
+
+            //     if(array_key_exists($i,$outdoors))
+            //     {
+            //         $featuresList->id_outdoor = $outdoors[$i];
+            //     }
+            //     if(array_key_exists($i,$hygienes))
+            //     {
+            //        $featuresList->id_hygiene = $hygienes[$i];
+            //     }
+            //     $featuresList->save();
+
+            // }
 
             //return successful response
-              return response()->json(['property' => $property,'property_type' => $propertyType, 'room' => $room,'message' => 'CREATED'], 201);
+            // return response()->json(['property' => $property,'property_type' => $propertyType, 'room' => $room,'message' => 'CREATED'], 201);
 
         } catch (\Exception $e) {
             //return error message
