a7482785145faf47cef370ea70ec88b014d7340b
bakhtaouiines <inesbkht@gmail.com>
2022-02-03 14:22:54 +0100

    upload method finished, storage in DB ok

--- blog/app/Models/ClientDocument.php
+++ blog/app/Models/ClientDocument.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ClientDocument extends Model
 {
     protected $table = 'client_document';
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_client', 'path'
+    ];
 }
