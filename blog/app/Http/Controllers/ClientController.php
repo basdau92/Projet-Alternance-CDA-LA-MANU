072337afdb71f1915cae5794d081860f35eba87e
bakhtaouiines <inesbkht@gmail.com>
2022-02-28 16:29:10 +0100

    upload ok + working on reading doc

--- blog/app/Http/Controllers/ClientController.php
+++ blog/app/Http/Controllers/ClientController.php
@@ -22,7 +22,7 @@ class ClientController extends Controller
     }
 
     /**
-     * Get one client.
+     * Get one client by id 
      *
      * @param  int  $id
      * @return Response
@@ -83,51 +83,62 @@ class ClientController extends Controller
     }
 
     /**
-     * Authorize the client to upload document and store them in DB
+     * Authorize the client to upload documents and store them in DB
      * 
      * still need to define the max size of the file
      *
      * @param Request $request
      * @return Response
      */
-    public function uploadDocument(Request $request)
+    public function upload(Request $request)
     {
+        if (!$request->hasFile('document')) { // ensure the request has a file
+            return response()->json('Aucun fichier n\'a été trovué!', 400); // client side error, Bad Request
+        }
+
         try {
+            $files = $request->file('document'); // retrieve file from the request
+            $id_client = $request->input('id_client');
 
-            ($request->hasFile('document')); // ensure the request has a file
+            foreach ($files as $file) {
+                $extensionArray = ['jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'odt']; // Authorized extension
+                $filename = $file->hashName(); // Generate a unique, random name 
+                $file_ext = $file->extension(); // Determine the file's extension based on the file's MIME type
+                $document = time() . '.' . $filename; //add timestamp to the filename
+                $destination_path = storage_path('client');
+                $check = in_array(strtolower($file_ext), $extensionArray);
 
-            $extensionArray = array('jpg', 'png', 'jpeg', 'pdf', 'doc', 'docx', 'odt'); // Authorized extension
-            $file = $request->file('document'); // Retrieve file from the request
-            $filename = $file->hashName(); // Generate a unique, random name 
-            $file_ext = $file->extension(); // Determine the file's extension based on the file's MIME type
-            $document = time() . '.' . $filename;
-            $destination_path = storage_path('client'); // Save the file locally in the storage/client folder
+                if ($check) {
+                    $file->move($destination_path, $document); // move the file to storage/client
 
-            if (in_array(strtolower($file_ext), $extensionArray)) {
-
-                $clientDocument = new ClientDocument();
-                $clientDocument->id_client = Auth::userOrFail()->id;
-
-                $request->file('document')->move($destination_path, $document); // move the file to storage/client
-                $clientDocument->name = $filename;
-                $clientDocument->path = $destination_path;
-                $clientDocument->save();
-
-                return response()->json(['client_document' => $clientDocument, 'message' => 'Le fichier a bien été uploadé !'], 201);
-            } else {
-                return response()->json('L\'extension du fichier n\'est pas autorisée!', 403);
+                    $clientDocument = new ClientDocument();
+                    $clientDocument->id_client = Auth::userOrFail()->id;
+                    $clientDocument->name = $filename;
+                    $clientDocument->path = $destination_path;
+                    $clientDocument->id_client = $id_client;
+                    $clientDocument->save();
+                } else {
+                    return response()->json('L\'extension du fichier n\'est pas autorisée!', 403);
+                }
             }
-        } catch (\Exception $e) {
+            $result = clientDocument::where('id_client', $id_client)->get();
 
-            return response()->json(['message' => 'Le fichier ne peut pas être uploadé !', 'error' => $e->getMessage()], 409);
+            return response()->json(['document' => $result, 'message' => 'File uploaded !'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Le fichier ne peut pas être uploadé!', 'error' => $e->getMessage()], 409);
         }
     }
 
-    public function readDocument()
+    /**
+     * Retrieve all the documents by client's id
+     */
+    public function readFiles()
     {
         try {
-            $clientDocument = new ClientDocument();
-            $clientDocument->id_client = Auth::userOrFail()->id;
+            $document = ClientDocument::with((['showDocument']))
+                ->where('id_client', Auth::userOrFail()->id)
+                ->get();
+            dd($document);
 
             $directory = storage_path('client'); // fetch of the storage folder
             $data = $clientDocument->path = $directory;
@@ -146,16 +157,29 @@ class ClientController extends Controller
         }
     }
 
-    public function readSingleDocument()
+    /**
+     * Retrieve A document by its id
+     */
+    public function readSingleDocument($id)
     {
         try {
             $document = ClientDocument::with((['showDocument']))
                 ->where('id_client', Auth::userOrFail()->id)
                 ->get();
-            return response()->json(['document' => $document], 200);
+            $document = ClientDocument::findOrFail($id); // retrieve A document from its id
+
+            $directory = storage_path('client'); // fetch of the storage folder
+            $data = $document->path = $directory;
+            $files = File::files($data); // retrieving files 
+
+            foreach ($files as $file) {
+                $content = $file->getContents();
+            }
+            $encode = base64_encode($content); // encoding content of the file into string 
+            return response()->json(['document' => $encode], 200);
         } catch (\Exception $e) {
 
-            return response()->json(['message' => 'Le document n\'a pas été trouvé!', 'error' => $e->getMessage()], 404);
+            return response()->json(['message' => 'Le document n\'a pas été trouvé!'], 404);
         }
     }
 
