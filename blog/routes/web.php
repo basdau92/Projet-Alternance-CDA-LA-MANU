d39f3abc8ab578f150da6a5c18f4a352da687ea2
bakhtaouiines <inesbkht@gmail.com>
2022-03-01 09:24:58 +0100

    add updateFile route

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -27,10 +27,11 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
 $router->group(['prefix' => 'client'], function () use ($router) {
 
     $router->get('/my-favorites', 'FavoriteListController@showFavoriteList');
-    $router->post('/document', 'ClientController@upload');
-    $router->delete('/document/{id}', 'ClientController@deleteDocument');
+    $router->post('/documents', 'ClientController@upload');
+    $router->delete('/my-documents/{id}', 'ClientController@deleteFile');
+    $router->put('/my-documents/{id}', 'ClientController@updateFile');
     $router->get('/my-documents', 'ClientController@readFiles');
-    $router->get('/my-document/{id}', 'ClientController@readSingleDocument');
+    // $router->get('/my-document/{id}', 'ClientController@readSingleDocument');
     $router->get('/{id}', 'ClientController@singleClient');
     $router->get('/', 'ClientController@allClients');
     $router->delete('/{id}', 'ClientController@deleteClient');
