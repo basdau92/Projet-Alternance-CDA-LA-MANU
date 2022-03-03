4c508e8a0deed5f631195dcb540ed4f4a42e7660
bakhtaouiines <inesbkht@gmail.com>
2022-03-03 11:31:16 +0100

    working on mailing exceptions

--- blog/app/Exceptions/Handler.php
+++ blog/app/Exceptions/Handler.php
@@ -2,12 +2,17 @@
 
 namespace App\Exceptions;
 
+use Throwable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Throwable;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\ErrorHandler\ErrorRenderer\HtmlErrorRenderer;
 
 class Handler extends ExceptionHandler
 {
@@ -35,6 +40,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
+        // it will have access to the $error that we are passing bel
+        if ($this->shouldReport($exception)) {
+            $this->sendEmail($exception); // here we send an email
+        }
         parent::report($exception);
     }
 
@@ -50,5 +59,26 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Send an email when any exception is reported
+     * 
+     */
+    public function sendEmail(Throwable $exception)
+    {
+        try {
+            $e = FlattenException::create($exception);
+            $handler = new HtmlErrorRenderer(true);
+            $css = $handler->getStylesheet();
+            $content = $handler->getBody($e);
+
+            \Mail::send('emails.exception', compact('css', 'content'), function ($message) {
+                $message->to(['inesbkht@gmail.com', 'inesbkht@gmail.com'])
+                    ->subject('Exception: ' . \Request::fullUrl());
+            });
+        } catch (Throwable $exception) {
+            Log::error($exception);
+        }
     }
 }
---
+++ blog/resources/views/emails/exception.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>There was an error in your Laravel App</title>
+</head>
+<body>
+    <p>{$content}</p>
+</body>
+</html
