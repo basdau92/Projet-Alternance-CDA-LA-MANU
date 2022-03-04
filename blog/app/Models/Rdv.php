1299dbc9782f590a81fa3eaf9ec7d30ab7235f75
bakhtaouiines <inesbkht@gmail.com>
2022-03-04 16:15:29 +0100

    remove useless lines

--- blog/app/Models/Rdv.php
+++ blog/app/Models/Rdv.php
@@ -2,12 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class Rdv extends Model
 {
