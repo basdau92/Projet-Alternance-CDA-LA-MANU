4d914816b6686e02b07aab85d85734bd09cc72a5
bakhtaouiines <inesbkht@gmail.com>
2022-05-03 14:20:06 +0200

    custom error messages

---
+++ api/resources/lang/fr/validation.php
@@ -0,0 +1,34 @@
+<?php
+return [
+    'custom' => [
+        'password' => [
+            'required' => 'Le mot de passe est obligatoire',
+            'min' => 'Le mot de passe doit contenir au moins :min caractères.',
+            'regex' =>  'Votre mot de passe doit contenir au moins 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial.',
+        ]
+    ],
+    'mail' => [
+        'required' => 'Le mail est obligatoire',
+        'email' => 'Veuillez vérifier le format de votre mail.',
+        'unique' => 'Cette adresse mail existe déjà.'
+    ],
+    'lastname' => [
+        'required' => 'Le nom de famille est obligatoire.',
+        'string' => 'Le nom de famille ne peut être composé que de lettres.'
+    ],
+    'firstname' => [
+        'required' => 'Le prénom est obligatoire.',
+        'string' => 'Le prénom ne peut être composé que de lettres.'
+    ],
+    'phone' => [
+        'required' => 'Le téléphone est obligatoire.',
+        'numeric' => 'Le téléphone ne peut être composé que de chiffres.'
+    ],
+    'id_agency' => [
+        'required' => 'Le n° de l\'agence est obligatoire.',
+        'numeric' => 'Le n° de l\'agence ne peut être composé que de chiffres.'
+    ],
+    'idNUmber' => [
+        'unique' => 'Ce numéro d\'employé existe déjà.'
+    ]
+];
