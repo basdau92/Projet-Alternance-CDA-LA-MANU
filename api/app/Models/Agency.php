a22482791ea92c6f76f9988dc385d8e81641db1c
bakhtaouiines <inesbkht@gmail.com>
2022-08-31 11:28:47 +0200

    add missing agency model

---
+++ api/app/Models/Agency.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Agency extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'address',
+        'zipcode',
+        'mail',
+        'phone',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Define table that should be used by the model.  
+     */
+    protected $table = 'agency';
+}
